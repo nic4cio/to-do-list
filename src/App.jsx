5ccd24072e5f514207db654a941bdbd1b995e9fd
nic4cio <gabrielnn_1@live.com>
2023-08-14 19:54:33 -0300

    useState to store an array of task objects

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,10 @@
+import { useState } from 'react'
 import './App.css'
 import CardAdicionar from './components/CardAdicionar'
 
 function App() {
 
+  const [listaTarefas, setListaTarefas] = useState([{textoTarefa: "tarefa 1", finalizado: false}])
   return (
     <>
       <CardAdicionar/>
